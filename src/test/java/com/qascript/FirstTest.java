60f45f47751a136139fbd3fcb025ce32fa4ec3d3
simitaws <75500146+simitaws@users.noreply.github.com>
2021-10-18 13:22:19 -0400

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
 
 public class FirstTest {
 
