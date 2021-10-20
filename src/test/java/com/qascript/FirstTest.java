e7fd41a8536bd17c06ede2a13a3b3fbe866d052c
simitaws <75500146+simitaws@users.noreply.github.com>
2021-10-20 08:37:14 -0400

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -16,6 +16,7 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
 /*        WebDriver driver;   */
+	    Console console = System.console();
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\SimitBhattacharyya\\OneDrive - iCatalyst Inc\\Desktop\\Selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://3.144.243.162:8081/v1/docker");
