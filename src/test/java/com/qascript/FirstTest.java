60ea82b6eccd167aed74cf9019beba6531206334
simitaws <75500146+simitaws@users.noreply.github.com>
2021-10-21 12:40:29 -0400

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -8,24 +8,30 @@ import org.testng.annotations.Test;
  
 public class FirstTest {
  
-        public static void main(String[] args) throws IOException, InterruptedException {
-                System.setProperty("webdriver.chrome.driver", "C:\\Users\\SimitBhattacharyya\\OneDrive - iCatalyst Inc\\Desktop\\Selenium\\chromedriver.exe");
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--headless");
-                chromeOptions.addArguments("--no-sandbox");
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
  
-                WebDriver driver = new ChromeDriver(chromeOptions);
- 
-                driver.get("https://google.com");
- 
-                Thread.sleep(1000);
- 
-                if (driver.getPageSource().contains("I'm Feeling Lucky")) {
-                        System.out.println("Pass");
-                } else {
-                        System.out.println("Fail");
-                }
-                driver.quit();
-        }
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\SimitBhattacharyya\\OneDrive - iCatalyst Inc\\Desktop\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://3.144.243.162:8081/v1/docker");
+		String testString = "Welcome to deployment in docker DOED TEST"; 
+		
+/*		String testSample =driver.findElement(By.tagName("h1")).getText(); */
+		String testSample =driver.findElement(By.tagName("body")).getText();
+ 	
+		System.out.println("Text= " +testSample);
+		  if (testSample.equals(testString))
+		  { 
+		  System.out.println("Success");
+		   System.out.println(testSample.getClass().getSimpleName());
+		   System.exit(0);
+		   }
+		  else
+			{System.out.println("Failure");
+			System.exit(1);
+			}
+		
+	}
+
 }
 
