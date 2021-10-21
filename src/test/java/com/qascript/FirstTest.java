435405f5aecbbf897c7132d0472c0005c801b6a8
simitaws <75500146+simitaws@users.noreply.github.com>
2021-10-21 12:54:25 -0400

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,7 +1,10 @@
-package test.jenkins.selenium;
-import java.io.IOException;
-/* import org.testng.annotations.Test;  */
-import org.openqa.selenium.By;
+package com.qascript;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
  
 public class FirstTest {
  
