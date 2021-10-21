841b2475e9ab763e6d973e86e7ce4364e6e61442
simitaws <75500146+simitaws@users.noreply.github.com>
2021-10-21 14:33:42 -0400

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -17,7 +17,7 @@ public class FirstTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://3.144.243.162:8081/v1/docker");
 /*		String testString = "index.html";  */
-	         String testString = "index.htm"; 
+	         String testString = "index.html"; 
 		
 /*		String testSample =driver.findElement(By.tagName("h1")).getText(); */
 		String testSample =driver.findElement(By.tagName("body")).getText();
