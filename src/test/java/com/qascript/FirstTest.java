0db0a2668318a75ae14425bdd569b038fab1ec13
simitaws <75500146+simitaws@users.noreply.github.com>
2021-10-22 12:25:34 -0400

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -15,22 +15,42 @@ public class FirstTest {
 /*        WebDriver driver;   */
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://3.144.243.162:8081/v1/docker");
-/*		String testString = "index.html";  */
-	         String testString = "index.html"; 
+		driver.get("http://18.219.96.155:8081/");
+		String testString = "Enter Name"; 
+		String testString1 = "Enter Correspondence"; 
 		
 /*		String testSample =driver.findElement(By.tagName("h1")).getText(); */
-		String testSample =driver.findElement(By.tagName("body")).getText();
+		String testSample =driver.findElement(By.tagName("h2")).getText();
+		String testSample1 =driver.findElement(By.tagName("h1")).getText();
+/*		String testSample =driver.findElement(By.tagName("body")).getText();  */
  	
-		System.out.println("Text= " +testSample);
+/*                    TEST #1                                */
+        	System.out.println("Text= " +testSample);
 		  if (testSample.equals(testString))
 		  { 
 		  System.out.println("Success");
-		   System.out.println(testSample.getClass().getSimpleName());}
+		   System.out.println(testSample.getClass().getSimpleName());
+/*		   System.exit(0);     */
+		   }
 		  else
+			{
 			System.out.println("Failure");
-		
-	}
+/*			System.exit(1);     */
+			}    
+		  
+/*                    TEST #2                                 */
+		System.out.println("Text= " +testSample1);
+		   if (testSample1.equals(testString1))
+			{ 
+			 System.out.println("Success");
+			 System.out.println(testSample1.getClass().getSimpleName());
+/*			 System.exit(0);     */
+			 }
+		   else
+		   {
+		   System.out.println("Failure");
+/*		   System.exit(1);       */
+		   } 
 
 
 }
